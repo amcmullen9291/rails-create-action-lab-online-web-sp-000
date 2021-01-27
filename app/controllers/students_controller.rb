f7fd8fcb57a443d3ba6c50d62447cd2e1c5259c7
anthony mcmullen <amcmullen9291@gmail.com>
2021-01-27 08:35:50 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
-    redirect_to 'student#new'
+    redirect_to 'students#show'
   end
 
 end
